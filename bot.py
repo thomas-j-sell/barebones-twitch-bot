470bf16407e86bc9105db1097a4944c5545d7ea1
Tom Sell <thomas.j.sell@gmail.com>
2020-04-19 19:52:16 -0400

    upgrading hello recognition

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+""" bot.py Main file for teaja_bot. Puts everything together and sets up main loop. """
+
 import os
 from config import bot, env_prefix
 
@@ -5,11 +7,13 @@ import general_commands
 import joke_commands
 import queue_commands
 
+
 # bot specific commands
 if(env_prefix == 'TEAJA_'):
     import teaja_commands
 elif(env_prefix == 'CELERY_'):
     import celery_commands
+
 
 # add the list of available commands, all commands need to be imported before this
 @bot.command(name='commands')
@@ -20,10 +24,11 @@ async def commands(ctx):
     for key in command_list:
         command_list_str += f"!{key}, "
 
-    # hack to remove the last comma
+    # remove the last comma
     command_list_str = command_list_str[:-1]
 
     await ctx.send(f"Available commands: {command_list_str}")
+
 
 @bot.event
 async def event_ready():
@@ -32,6 +37,7 @@ async def event_ready():
     print(f"{nickname} is online!")
     ws = bot._ws  # this is only needed to send messages within event_ready
     await ws.send_privmsg(os.environ[f"{env_prefix}CHANNEL"], f"/me Hello. I am {nickname}. Type !commands to see what I can do.")
+
 
 @bot.event
 async def event_message(ctx):
@@ -43,12 +49,17 @@ async def event_message(ctx):
 
     await bot.handle_commands(ctx)
 
-    # await ctx.channel.send(ctx.content)
 
-    #TODO respond to a variety of hello, hey, hi, sup, etc.
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+    # respond to a variety of hello, hey, hi, sup, etc.
+    hellos = ['hello', 'hey', 'hi', 'sup', 'whatsup', 'ello', 'hawwo', 'howdy', 'yo']
+    # remove special characters and convert to lower case to compare to list
+    message = ''.join(filter(str.isalnum, ctx.content.lower()))
 
+    if message in hellos:
+        await ctx.channel.send(f"{ctx.content} @{ctx.author.name}!")
+
+
+    # if anyone types '^' this bot will copy it
     if ctx.content.lower() == '^':
         await ctx.channel.send('^')
 
