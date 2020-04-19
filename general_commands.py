8a4e85f36a292fc995a9c5bf7fbedb6170e0a57d
Tom Sell <thomas.j.sell@gmail.com>
2020-04-19 18:14:59 -0400

    fixing !source URL

--- general_commands.py
+++ general_commands.py
@@ -2,7 +2,7 @@ from config import bot
 
 @bot.command(name='source')
 async def source(ctx):
-    await ctx.send('I am a bot created by Dredgen_Teaja. You can see the code that makes me work at https://github.com/thomas-j-sell')
+    await ctx.send('I am a bot created by Dredgen_Teaja. You can see the code that makes me work at https://github.com/thomas-j-sell/teaja-bot')
 
 @bot.command(name='blurg')
 async def blurg(ctx):
