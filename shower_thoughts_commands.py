aaff20d93df069eae2c641e99678b36d6bb28689
Tom Sell <thomas.j.sell@gmail.com>
2020-07-31 22:39:23 -0400

    adding more shower thoughts

--- shower_thoughts_commands.py
+++ shower_thoughts_commands.py
@@ -14,16 +14,30 @@ async def showerthought(ctx):
             else:
                 await ctx.send(f"Sorry {ctx.author.name}, that number is out of the range of my shower thoughts list.")
         elif words[1].lower() == 'size' or words[1].lower() == 'length':
-            await ctx.send(f"There are {len(thoughts)} thoughts in my list. So valid joke indexes are 0-{len(thoughts)-1}.")
+            await ctx.send(f"There are {len(thoughts)} thoughts in my list. So valid thought indexes are 0-{len(thoughts)-1}.")
     else:
         await ctx.send(random.choice(thoughts))
 
-# the dad-a-base
+# * scratches chin * hmmmmmmmm
 thoughts = [
         'No matter how many lasagnas you stack on top of one another, you still only have 1 lasagna.',
         'CPR is just the human version of "blowing on a game cardtridge".',
         'Aliens invaded the moon on July 20, 1969.',
         'Why is it that we park on a driveway, but drive on a parkway?',
         'Why are pizzas round, but we cut them into triangles and put them in a square box?',
-        "Why is it that when we send something by boat it's called cargo, but when we send it by car it's called a shipment?"
+        "Why is it that when we send something by boat it's called cargo, but when we send it by car it's called a shipment?",
+        "If a house has solar panels and AC, the sun is simultaneously heating and cooling it.",
+        "If it hasn't been cold before then technically it can only be fridgerated.",
+        "How did a fool and his money get together in the first place?",
+        "What monster put the cat in a bag to begin with?",
+        "Why do we call it a double U when it's clearly a double V?",
+        'Why does "fridge" have a D in it but "refrigerator" does not?',
+        "When Sweden plays against Denmark in sports, the scoreboard will say Swe-Den and the unused letters will be Den-Mark.",
+        'Why do they make "birthday cake" flavored things when birthday cakes can be any flavor?',
+        "Onion rings are just vegatable donuts.",
+        "Your stomach thinks all potatoes are mashed.",
+        "Lobsters are mermaids to scorpions.",
+        "Cookie dough is the sushi of deserts.",
+        "If tomatoes are fruits then isn't ketchup a jelly?",
+        "Two wrongs don't make a right, but three lefts do."
  ]
